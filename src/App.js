83a7764e5b17879323db91fdb79353954af9f809
spy <espi8624@gmail.com>
2023-10-24 22:25:36 +0900

    project reservation web start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+
+function NavigationBar() {
+  return (
+    <>
+      <div className='navigation-bar'>
+        <div className='nav-button-l'>Menu</div>
+        <div className='nav-button-r'>
+          <ul>
+            <li><Link to={'/reservation'}>예약현황</Link></li>
+            <li>Notice</li>
+          </ul>
+        </div>
+      </div>
+      <div className='white-space'></div>
+    </>
+
+  );
+}
+
+function ReservationPage() {
+  return (
+    <>
+      <div className="content">
+        <div>가게이름</div>
+        <div>예약 내용</div>
+        <div>예약일</div>
+        <div>예약 시간</div>
+        <div>장소</div>
+        <div>담당자</div>
+      </div>
+
+      <div className="content">
+        예약 이력
+      </div>
+
+      <div className="content">
+        지나간 예약 이력
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavigationBar />
+        <Routes>
+          <Route path='/reservation' element={<ReservationPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
