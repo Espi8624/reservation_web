f156a29cd5f30bf7c26163020dd5bd2fcf3113cf
spy <espi8624@gmail.com>
2023-11-30 23:15:03 +0900

    ready for repair nav bar

--- src/App.js
+++ src/App.js
@@ -8,16 +8,15 @@ import ReservationCheckPage from './components/ReservationCheckPage';
 function NavigationBar() {
   return (
     <>
-      <div className='navigation-bar'>
-        <div className='nav-button-l'>メニュー</div>
-        <div className='nav-button-r'>
+      <div className='navigation-bar-wrap'>
+        <div className='navigation-bar-title'>メニュー</div>
+        <div className='navigation-bar'>
           <ul>
-            <li><Link to={'/chkreservation'}>予約状況</Link></li>
+            <li>予約状況</li>
             <li>お知らせ</li>
           </ul>
         </div>
       </div>
-      <div className='white-space'></div>
     </>
 
   );
@@ -26,11 +25,11 @@ function NavigationBar() {
 function App() {
   return (
     <BrowserRouter>
+      <NavigationBar />
       <div className="App">
-        <NavigationBar />
         <Routes>
-          <Route path='/' element={<MainPage/>}></Route>
-          <Route path='/chkreservation' element={<ReservationCheckPage/>}/>
+          <Route path='/' element={<MainPage />}></Route>
+          <Route path='/chkreservation' element={<ReservationCheckPage />} />
         </Routes>
       </div>
     </BrowserRouter>
